2a6204111728a98720401a5447ff7979fd9f3816
mathrews <mateusps4gg@gmail.com>
2026-01-05 15:47:18 -0300

    Add: Colunas agora condizentes com os dados vindos do backend. ToDo: verificar a logica de negocio dos telefones no front e no backend

--- src/pages/ClientTablePage.tsx
+++ src/pages/ClientTablePage.tsx
@@ -1,12 +1,17 @@
-import { useState, useEffect } from 'react';
-import { DataTable } from 'primereact/datatable';
-import { Column } from 'primereact/column';
-import { Button } from 'primereact/button';
-import type { Client, ClientCreate } from '../types/Client';
-import { getClients, deleteClient, createClient, updateClient } from '../db_api/db_api';
-import { Dialog } from 'primereact/dialog';
-import { InputText } from 'primereact/inputtext';
-import {InputNumber} from 'primereact/inputnumber';
+import { useState, useEffect } from "react";
+import { DataTable } from "primereact/datatable";
+import { Column } from "primereact/column";
+import { Button } from "primereact/button";
+import type { Client, ClientCreate } from "../types/Client";
+import {
+  getClients,
+  deleteClient,
+  createClient,
+  updateClient,
+} from "../db_api/db_api";
+import { Dialog } from "primereact/dialog";
+import { InputText } from "primereact/inputtext";
+import { InputNumber } from "primereact/inputnumber";
 
 const ClientTablePage = () => {
   const [clients, setClients] = useState<Client[]>([]);
@@ -14,15 +19,15 @@ const ClientTablePage = () => {
   const [showDialog, setShowDialog] = useState(false);
 
   const [novoCliente, setNovoCliente] = useState<ClientCreate>({
-    nome: '',
+    nome: "",
     idade: 0,
-    cpf: '',
-    email: '',
-    endereco: '',
-    cidade: '',
-    estado: '',
-    data_nascimento: '',
-    telefones: ['', ''],
+    cpf: "",
+    email: "",
+    endereco: "",
+    cidade: "",
+    estado: "",
+    data_nascimento: "",
+    telefones: ["", ""],
     ativo: true,
   });
 
@@ -38,7 +43,7 @@ const ClientTablePage = () => {
       const newClients = await getClients(ativos);
       setClients(newClients);
     } catch (e) {
-      console.error('Falha ao deletar:', e);
+      console.error("Falha ao deletar:", e);
     }
   };
 
@@ -46,7 +51,7 @@ const ClientTablePage = () => {
     const clienteForm = editCliente || novoCliente;
 
     if (!clienteForm.nome || !clienteForm.email) {
-      alert('Nome e email são obrigatórios!');
+      alert("Nome e email são obrigatórios!");
       return;
     }
 
@@ -54,13 +59,15 @@ const ClientTablePage = () => {
       if (editCliente) {
         // Atualiza cliente existente
         await updateClient(editCliente.id, editCliente);
-        setClients(prev =>
-          prev.map(c => (c.id === editCliente.id ? { ...c, ...editCliente } : c))
+        setClients((prev) =>
+          prev.map((c) =>
+            c.id === editCliente.id ? { ...c, ...editCliente } : c
+          )
         );
       } else {
         // Cria novo cliente
         await createClient(novoCliente);
-        setClients(prev => [
+        setClients((prev) => [
           ...prev,
           {
             ...novoCliente,
@@ -73,27 +80,31 @@ const ClientTablePage = () => {
 
       setShowDialog(false);
       setNovoCliente({
-        nome: '',
+        nome: "",
         idade: 0,
-        cpf: '',
-        email: '',
-        endereco: '',
-        cidade: '',
-        estado: '',
-        data_nascimento: '',
-        telefones: ['', '']
+        cpf: "",
+        email: "",
+        endereco: "",
+        cidade: "",
+        estado: "",
+        data_nascimento: "",
+        telefones: ["", ""],
       });
 
       setEditCliente(null); // Limpa edição após salvar
     } catch (e) {
-      console.error('Erro ao salvar cliente:', e);
-      alert('Falha ao salvar cliente!');
+      console.error("Erro ao salvar cliente:", e);
+      alert("Falha ao salvar cliente!");
     }
   };
 
   const actionBodyTemplate = (rowData: Client) => (
-    <div style={{ display: 'flex', gap: '20px' }}>
-      <Button icon="pi pi-trash" className="p-button-danger" onClick={() => handleDelete(rowData)} />
+    <div style={{ display: "flex", gap: "20px" }}>
+      <Button
+        icon="pi pi-trash"
+        className="p-button-danger"
+        onClick={() => handleDelete(rowData)}
+      />
       <Button
         icon="pi pi-pencil"
         className="p-button-success"
@@ -115,45 +126,53 @@ const ClientTablePage = () => {
           type="checkbox"
           name="checkativos"
           checked={ativos}
-          onChange={async e => setAtivos(e.target.checked)}
+          onChange={async (e) => setAtivos(e.target.checked)}
         />
       </label>
 
-      <div style={{ display: 'flex', gap: '20px', height: '8rem', alignItems: 'center' }}>
+      <div
+        style={{
+          display: "flex",
+          gap: "20px",
+          height: "8rem",
+          alignItems: "center",
+        }}
+      >
         <h1>Tabela de Clientes</h1>
         <Button
-        label="Adicionar"
-        onClick={() => {
-          setNovoCliente({
-            nome: '',
-            idade: 0,
-            cpf: '',
-            email: '',
-            endereco: '',
-            cidade: '',
-            estado: '',
-            data_nascimento: '',
-            telefones: ['', '']
-          });
+          label="Adicionar"
+          onClick={() => {
+            setNovoCliente({
+              nome: "",
+              idade: 0,
+              cpf: "",
+              email: "",
+              endereco: "",
+              cidade: "",
+              estado: "",
+              data_nascimento: "",
+              telefones: ["", ""],
+            });
 
-          setEditCliente(null);
-          setShowDialog(true);
-        }}
+            setEditCliente(null);
+            setShowDialog(true);
+          }}
         />
       </div>
 
       <Dialog
-        header={editCliente ? 'Editar Cliente' : 'Novo Cliente'}
+        header={editCliente ? "Editar Cliente" : "Novo Cliente"}
         visible={showDialog}
-        style={{ width: '400px' }}
+        style={{ width: "400px" }}
         onHide={() => setShowDialog(false)}
       >
         <div className="p-fluid">
           <label>Nome</label>
           <InputText
             value={clienteForm.nome}
-            onChange={e => {
-              if (editCliente) setEditCliente({ ...editCliente, nome: e.target.value });
+            onChange={(e) => {
+              if (editCliente)
+                setEditCliente({ ...editCliente, nome: e.target.value });
               else setNovoCliente({ ...novoCliente, nome: e.target.value });
             }}
           />
@@ -161,17 +180,19 @@ const ClientTablePage = () => {
           <label>Idade</label>
           <InputNumber
             value={clienteForm.idade}
-            onChange={e => {
-              if (editCliente) setEditCliente({ ...editCliente, idade: (e.value || 0) });
-              else setNovoCliente({ ...novoCliente, idade: (e.value || 0) });
+            onChange={(e) => {
+              if (editCliente)
+                setEditCliente({ ...editCliente, idade: e.value || 0 });
+              else setNovoCliente({ ...novoCliente, idade: e.value || 0 });
             }}
-	   />
+          />
 
           <label>CPF</label>
           <InputText
             value={clienteForm.cpf}
-            onChange={e => {
-              if (editCliente) setEditCliente({ ...editCliente, cpf: e.target.value });
+            onChange={(e) => {
+              if (editCliente)
+                setEditCliente({ ...editCliente, cpf: e.target.value });
               else setNovoCliente({ ...novoCliente, cpf: e.target.value });
             }}
           />
@@ -179,8 +200,9 @@ const ClientTablePage = () => {
           <label>Email</label>
           <InputText
             value={clienteForm.email}
-            onChange={e => {
-              if (editCliente) setEditCliente({ ...editCliente, email: e.target.value });
+            onChange={(e) => {
+              if (editCliente)
+                setEditCliente({ ...editCliente, email: e.target.value });
               else setNovoCliente({ ...novoCliente, email: e.target.value });
             }}
           />
@@ -188,8 +210,9 @@ const ClientTablePage = () => {
           <label>Endereço</label>
           <InputText
             value={clienteForm.endereco}
-            onChange={e => {
-              if (editCliente) setEditCliente({ ...editCliente, endereco: e.target.value });
+            onChange={(e) => {
+              if (editCliente)
+                setEditCliente({ ...editCliente, endereco: e.target.value });
               else setNovoCliente({ ...novoCliente, endereco: e.target.value });
             }}
           />
@@ -197,8 +220,9 @@ const ClientTablePage = () => {
           <label>Cidade</label>
           <InputText
             value={clienteForm.cidade}
-            onChange={e => {
-              if (editCliente) setEditCliente({ ...editCliente, cidade: e.target.value });
+            onChange={(e) => {
+              if (editCliente)
+                setEditCliente({ ...editCliente, cidade: e.target.value });
               else setNovoCliente({ ...novoCliente, cidade: e.target.value });
             }}
           />
@@ -206,8 +230,9 @@ const ClientTablePage = () => {
           <label>Estado</label>
           <InputText
             value={clienteForm.estado}
-            onChange={e => {
-              if (editCliente) setEditCliente({ ...editCliente, estado: e.target.value });
+            onChange={(e) => {
+              if (editCliente)
+                setEditCliente({ ...editCliente, estado: e.target.value });
               else setNovoCliente({ ...novoCliente, estado: e.target.value });
             }}
           />
@@ -215,19 +240,35 @@ const ClientTablePage = () => {
           <label>Data de Nascimento</label>
           <InputText
             value={clienteForm.data_nascimento}
-            onChange={e => {
-              if (editCliente) setEditCliente({ ...editCliente, data_nascimento: e.target.value });
-              else setNovoCliente({ ...novoCliente, data_nascimento: e.target.value });
+            onChange={(e) => {
+              if (editCliente)
+                setEditCliente({
+                  ...editCliente,
+                  data_nascimento: e.target.value,
+                });
+              else
+                setNovoCliente({
+                  ...novoCliente,
+                  data_nascimento: e.target.value,
+                });
             }}
           />
 
           <label>Telefone</label>
           <InputText
             value={clienteForm.telefones[0]}
-            onChange={e => {
-              if (editCliente) setEditCliente({
-                ...editCliente, telefones: [e.target.value, ''] });
-              else setNovoCliente({ ...novoCliente, telefones: [e.target.value, ''] });
+            onChange={(e) => {
+              if (editCliente) {
+                setEditCliente({
+                  ...editCliente,
+                  telefones: [e.target.value, ""],
+                });
+              } else {
+                setNovoCliente({
+                  ...novoCliente,
+                  telefones: [e.target.value, ""],
+                });
+              }
             }}
           />
 
@@ -235,13 +276,22 @@ const ClientTablePage = () => {
         </div>
       </Dialog>
 
-      <DataTable value={clients} paginator rows={10} stripedRows tableStyle={{ minWidth: '50rem' }}>
+      <DataTable
+        value={clients}
+        paginator
+        rows={10}
+        stripedRows
+        tableStyle={{ minWidth: "50rem" }}
+      >
         <Column field="id" header="ID" />
         <Column field="nome" header="Nome" />
+        <Column field="idade" header="Idade" />
+        <Column field="cpf" header="CPF" />
         <Column field="email" header="E-mail" />
-        <Column field="telefone" header="Telefone" />
-        <Column field="data_cadastro" header="Data de cadastro" />
+        <Column field="telefones" header="Telefone" />
+        <Column field="data_nascimento" header="Data de Nascimento" />
         <Column field="ativo" header="Ativo?" />
+        <Column field="endereco" header="Endereço" />
         <Column field="cidade" header="Cidade" />
         <Column field="estado" header="Estado" />
         <Column body={actionBodyTemplate} header="Ações" />
